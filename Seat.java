a601b6ddedc867616a75d5042d3e2a933aa47b33
SahanaPadaganur <152139466+SahanaPadaganur@users.noreply.github.com>
2024-03-04 19:35:38 +0530

    Update Seat.java

--- Seat.java
+++ Seat.java
@@ -10,5 +10,28 @@ class Seat{
         dateList=new String[20];
         //init();
     }
+    public int dateInd(String Date){ //Track dateList
+        this.Date=Date;
+        for(int i=0;i<dateList.length;i++){
+            if(dateList[i]==null || dateList[i].equals(Date)) return i;
+        }
+        return 0;
+    }
 
+    public int returnInd(int n,String d){ //return index of train if seats are booked in train
+        this.n=n;
+        for(int i=0;i<TN.length;i++){
+            if((TN[i][0]==n && TN[i][1]==dateInd(d)) || TN[i][0]==0) return i;
+        }
+        return 0;
+    }
+    public void showSeat(int t){
+        this.t=t;
+        int f=returnInd(n,Date);
+        for(int j=0;j<MAX;j++){
+            if(TRD[f][t][j]==1) continue;
+            System.out.print((j+1)+"\t");
+        }
+        System.out.println();
+    }
 }
