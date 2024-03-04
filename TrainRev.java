2ceedf4f118d1517004b4c3e857c01a1f8a456c1
snehahattaraki <152139263+snehahattaraki@users.noreply.github.com>
2024-03-04 19:23:41 +0530

    Update TrainRev.java

--- TrainRev.java
+++ TrainRev.java
@@ -28,3 +28,39 @@ class TrainRev{
          }
          return N; 
     }
+private int Stime(int ind){ //Generate arrival time
+        int dc=day.charAt(0)-'A'; 
+        int dec=dest.charAt(ind)-'a';
+        double hour=(((dc+dec)/2+1)*55/60.0)*100;
+        return (int)hour;
+    } 
+    private String[][] Time(){ //get array of time cooresponds to trainno
+        String time[][]=new String[3][2];
+        int temp[]=new int[2];
+        for(int j=0;j<3;j++){
+            temp[0]=Stime(j);temp[1]=T[j]+Stime(j);
+            for(int k=0;k<2;k++){
+                int t1=temp[k]/100;
+                int t2=temp[k]%100;
+                if(t2>60){
+                    t2=t2%60;
+                    t1++;
+                }
+                if(t1>23) t1=t1-23;
+                time[j][k]=t1+":"+t2;
+            }
+        }
+        return time;
+    }
+    public void showTrain(){
+        System.out.println("List of Trains:");
+        System.out.println("\tTrain No   \tArr Time\tDeparch Time");
+        for(int j=0;j<3;j++) System.out.println((j+1)+".    \t"+TrainNo()[j]+"    \t"+Time()[j][0]+"    \t"+Time()[j][1]);
+    }
+    public int getNo(int c){
+        return TrainNo()[c];
+    }
+    public String[] getTime(int c){
+        return Time()[c];
+    }
+}
